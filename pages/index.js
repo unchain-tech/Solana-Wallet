c463d8105369dbfaf0317fbb232e3115e2406e63
KatotenMk2 <cino1031@gmail.com>
2022-06-21 20:44:33 +0900

    Fixed by Katoten

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,53 @@
+import React from "react";
+import HeadComponent from '../components/Head';
+
 export default function Home() {
   return (
-    <div className="p-10">
-      <h1 className="text-5xl font-extrabold tracking-tight text-gray-900">
-        <span className="text-[#9945FF]">Solana</span>ウォレットを作ろう！
-      </h1>
-      <div className="mx-auto mt-5 text-gray-500">
-        Solanaウォレットの新規作成、インポート、エアドロップ、送金機能の開発にチャレンジしてみよう
-      </div>
+    <div>
+      <HeadComponent/>
+      <div className="p-10">
+        <h1 className="text-5xl font-extrabold tracking-tight text-gray-900">
+          <span className="text-[#9945FF]">Solana</span>ウォレットを作ろう！
+        </h1>
+        <div className="mx-auto mt-5 text-gray-500">
+          Solanaウォレットの新規作成、インポート、エアドロップ、送金機能の開発にチャレンジしてみよう
+        </div>
 
-      <hr className="my-6" />
+        <hr className="my-6" />
 
-      <div>
-        <h3 className="p-2 border-dotted border-l-8 border-l-indigo-600">My Wallet</h3>
-      </div>
+        <div>
+          <h3 className="p-2 border-dotted border-l-8 border-l-indigo-600">My Wallet</h3>
+        </div>
 
-      <hr className="my-6" />
+        <hr className="my-6" />
 
-      <div>
-        <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">STEP1: ウォレットを新規作成する</h2>
-      </div>
+        <div>
+          <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">STEP1: ウォレットを新規作成する</h2>
+        </div>
 
-      <hr className="my-6" />
+        <hr className="my-6" />
 
-      <div>
-        <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">STEP2: 既存のウォレットをインポートする</h2>
-      </div>
+        <div>
+          <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">STEP2: 既存のウォレットをインポートする</h2>
+        </div>
 
-      <hr className="my-6" />
+        <hr className="my-6" />
 
-      <div>
-        <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">STEP3: 残高を取得する</h2>
-      </div>
+        <div>
+          <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">STEP3: 残高を取得する</h2>
+        </div>
 
-      <hr className="my-6" />
+        <hr className="my-6" />
 
-      <div>
-        <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">STEP4: エアドロップ機能を実装する</h2>
-      </div>
+        <div>
+          <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">STEP4: エアドロップ機能を実装する</h2>
+        </div>
 
-      <hr className="my-6" />
+        <hr className="my-6" />
 
-      <div>
-        <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">STEP5: 送金機能を実装する</h2>
+        <div>
+          <h2 className="p-2 border-dotted border-l-4 border-l-indigo-400">STEP5: 送金機能を実装する</h2>
+        </div>
       </div>
     </div>
   )
