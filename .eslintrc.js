551fbfc9f72af55f68c4c71e610306eb9ee16852
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-04-17 10:10:55 +0900

    refactor: use prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,21 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["standard", "prettier", "plugin:react/recommended"],
+  extends: ['standard', 'prettier', 'plugin:react/recommended'],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
   rules: {
-    "no-lone-blocks": ["off"],
-    "no-unused-vars": ["warn"],
-    "react/prop-types": ["off"],
-    "react/jsx-uses-react": ["off"],
-    "react/react-in-jsx-scope": ["off"],
+    'no-lone-blocks': ['off'],
+    'no-unused-vars': ['warn'],
+    'react/prop-types': ['off'],
+    'react/jsx-uses-react': ['off'],
+    'react/react-in-jsx-scope': ['off'],
   },
   settings: {
     react: {
-      version: "18.0.0",
+      version: '18.0.0',
     },
   },
 };
